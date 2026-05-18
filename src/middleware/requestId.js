82e6f04dd9c0cfb465df75aa15eaa3b760640776
liyanchao <liyanchao@dcloud.io>
2026-05-18 11:13:37 +0800

    fix: 修改reqId使用headers中的x-request-id

--- src/middleware/requestId.js
+++ src/middleware/requestId.js
@@ -2,7 +2,7 @@ const crypto = require('crypto')
 const { asyncLocalStorage } = require('../utils/requestContext')
 
 const requestIdMiddleware = (req, res, next) => {
-  const reqId = crypto.randomBytes(4).toString('hex')
+  const reqId = req.headers['x-request-id'] || crypto.randomBytes(4).toString('hex')
   req.reqId = reqId
   res.setHeader('X-Relay-Request-Id', reqId)
   const sessionId = req.headers['session_id'] || req.headers['x-session-id'] || null
